3c40f5c251d31424880f50e072072d75c1fbc726
alex <alex.bertolla@gmail.com>
2020-10-24 12:28:47 -0300

    Filtros: Gaussiano, Mediana, Wiener e Non Local Means redimensionar imagens (256x256)

---
+++ redimensionar.py
@@ -0,0 +1,16 @@
+from skimage import img_as_float, img_as_ubyte
+from skimage.io import imread, imsave
+from skimage.transform import resize
+import os
+
+
+dir_imagens = 'imagens_originais'
+
+lista_imagens_originais = os.listdir(dir_imagens)
+for nome_imagem in lista_imagens_originais:
+    imagem_original = imread(dir_imagens + '/' + nome_imagem)
+    imagem_redimensionada = resize(imagem_original, (256, 256))
+    imagem_redimensionada = img_as_ubyte(imagem_redimensionada)
+    imsave(dir_imagens + '/' + nome_imagem, imagem_redimensionada)
+    print(dir_imagens + '/' + nome_imagem +' - '+ str(imagem_redimensionada.shape))
+print('FIM ADD RUIDO RESIZE')
