3ad6bb86d878e7b493d09351560a1ef84f75cfc9
alex <alex.bertolla@gmail.com>
2020-12-05 19:03:52 -0300

    05/12/2020 19:03 Atualização testes escolher melhor filtro

--- teste/teste_escolher_melhor_filtro.py
+++ teste/teste_escolher_melhor_filtro.py
@@ -38,6 +38,15 @@ def aplicar_filtro_passa_baixa(imagem, porcentagem_corte):
     imagem_filtrada = fp.ifft2(fftpack.ifftshift(shift_frq)).real
     return imagem_filtrada
 
+def aplicar_filtro_passa_alta(imagem, porcentagem_corte):
+    discrete_transform_imagem = fp.fft2(imagem)
+    (w, h) = discrete_transform_imagem.shape
+    half_w, half_h = int(w / 2), int(h / 2)
+    shift_frq = fftpack.fftshift(discrete_transform_imagem)
+    shift_frq[half_w - (int(half_w * porcentagem_corte)):half_w + (int(half_w * porcentagem_corte)) + 1, half_h - (int(half_h * porcentagem_corte)):half_h + (int(half_h * porcentagem_corte)) + 1] = 0
+    imagem_filtrada = np.clip(fp.ifft2(fftpack.ifftshift(shift_frq)).real, 0, 255)
+    return imagem_filtrada
+
 def add_ruido_gaussiano(imagem_original, sigma):
     imagem_ruido_gaussiano = random_noise(imagem_original, var=sigma)
     return imagem_ruido_gaussiano
@@ -47,15 +56,29 @@ lista_instensidade_ruido = [0.00, 0.05, 0.10, 0.15]
 dir_imagens_ruidosas = '../imagens_ruido_gaussiano/'
 dir_imagens_originais = '../banco_imagens/'
 
-lista_psnr_filtro_5 = []
-lista_psnr_filtro_7 = []
-lista_psnr_filtro_10 = []
-lista_psnr_filtro_13 = []
-lista_psnr_filtro_15 = []
+lista_psnr_filtro_5_pb = []
+lista_psnr_filtro_7_pb = []
+lista_psnr_filtro_10_pb = []
+lista_psnr_filtro_13_pb = []
+lista_psnr_filtro_15_pb = []
 
-lista_mse_filtro_5 = []
-lista_mse_filtro_10 = []
-lista_mse_filtro_15 = []
+lista_psnr_filtro_5_pa = []
+lista_psnr_filtro_7_pa = []
+lista_psnr_filtro_10_pa = []
+lista_psnr_filtro_13_pa = []
+lista_psnr_filtro_15_pa = []
+
+lista_mse_filtro_5_pb = []
+lista_mse_filtro_7_pb = []
+lista_mse_filtro_10_pb = []
+lista_mse_filtro_13_pb = []
+lista_mse_filtro_15_pb = []
+
+lista_mse_filtro_5_pa = []
+lista_mse_filtro_7_pa = []
+lista_mse_filtro_10_pa = []
+lista_mse_filtro_13_pa = []
+lista_mse_filtro_15_pa = []
 
 
 
@@ -67,7 +90,8 @@ for nome_imagem in lista_imagens:
     #print(imagem_original.dtype)
 
     imagem_ruidosa = img_as_ubyte(np.zeros(imagem_original.shape))
-    imagem_filtrada = img_as_ubyte(np.zeros(imagem_original.shape))
+    imagem_filtrada_pb = img_as_ubyte(np.zeros(imagem_original.shape))
+    imagem_filtrada_pa = img_as_ubyte(np.zeros(imagem_original.shape))
 
     #print(imagem_ruidosa.dtype)
     #print(imagem_filtrada.dtype)
@@ -143,115 +167,156 @@ for nome_imagem in lista_imagens:
     for porcentagem_corte in lista_porcentagem_corte:
         print('Porcentagem de corte: ' + str(porcentagem_corte * 100) + '%')
 
-        q1_filtrado = np.array(aplicar_filtro_passa_baixa(q1_ruidoso, porcentagem_corte), dtype='uint8')
-        q2_filtrado = np.array(aplicar_filtro_passa_baixa(q2_ruidoso, porcentagem_corte), dtype='uint8')
-        q3_filtrado = np.array(aplicar_filtro_passa_baixa(q3_ruidoso, porcentagem_corte), dtype='uint8')
-        q4_filtrado = np.array(aplicar_filtro_passa_baixa(q4_ruidoso, porcentagem_corte), dtype='uint8')
+        q1_filtrado_pb = np.array(aplicar_filtro_passa_baixa(q1_ruidoso, porcentagem_corte), dtype='uint8')
+        q2_filtrado_pb = np.array(aplicar_filtro_passa_baixa(q2_ruidoso, porcentagem_corte), dtype='uint8')
+        q3_filtrado_pb = np.array(aplicar_filtro_passa_baixa(q3_ruidoso, porcentagem_corte), dtype='uint8')
+        q4_filtrado_pb = np.array(aplicar_filtro_passa_baixa(q4_ruidoso, porcentagem_corte), dtype='uint8')
+
+        q1_filtrado_pa = np.array(aplicar_filtro_passa_alta(q1_ruidoso, porcentagem_corte), dtype='uint8')
+        q2_filtrado_pa = np.array(aplicar_filtro_passa_alta(q2_ruidoso, porcentagem_corte), dtype='uint8')
+        q3_filtrado_pa = np.array(aplicar_filtro_passa_alta(q3_ruidoso, porcentagem_corte), dtype='uint8')
+        q4_filtrado_pa = np.array(aplicar_filtro_passa_alta(q4_ruidoso, porcentagem_corte), dtype='uint8')
 
         #print(q1_filtrado.dtype)
         #print(q2_filtrado.dtype)
         #print(q3_filtrado.dtype)
         #print(q4_filtrado.dtype)
 
-        imagem_filtrada[:int(l / 2), :int(c / 2)] = q1_filtrado
-        imagem_filtrada[:int(l / 2), int(c / 2):] = q2_filtrado
-        imagem_filtrada[int(l / 2):, :int(c / 2)] = q3_filtrado
-        imagem_filtrada[int(l / 2):, int(c / 2):] = q4_filtrado
+        imagem_filtrada_pb[:int(l / 2), :int(c / 2)] = q1_filtrado_pb
+        imagem_filtrada_pb[:int(l / 2), int(c / 2):] = q2_filtrado_pb
+        imagem_filtrada_pb[int(l / 2):, :int(c / 2)] = q3_filtrado_pb
+        imagem_filtrada_pb[int(l / 2):, int(c / 2):] = q4_filtrado_pb
+
+        imagem_filtrada_pa[:int(l / 2), :int(c / 2)] = q1_filtrado_pa
+        imagem_filtrada_pa[:int(l / 2), int(c / 2):] = q2_filtrado_pa
+        imagem_filtrada_pa[int(l / 2):, :int(c / 2)] = q3_filtrado_pa
+        imagem_filtrada_pa[int(l / 2):, int(c / 2):] = q4_filtrado_pa
 
         #imagem_ruidosa[:int(l / 2), :int(c / 2)] = q1_ruidoso
         #imagem_ruidosa[:int(l / 2), int(c / 2):] = q2_ruidoso
         #imagem_ruidosa[int(l / 2):, :int(c / 2)] = q3_ruidoso
         #imagem_ruidosa[int(l / 2):, int(c / 2):] = q4_ruidoso
 
-        array_psnr = []
-        array_psnr.append(calcular_psnr(q1_original, q1_filtrado))
-        array_psnr.append(calcular_psnr(q2_original, q2_filtrado))
-        array_psnr.append(calcular_psnr(q3_original, q3_filtrado))
-        array_psnr.append(calcular_psnr(q4_original, q4_filtrado))
-        mediana_psnr = round(np.median(array_psnr), 2)
+        array_psnr_pb = []
+        array_psnr_pb.append(calcular_psnr(q1_original, q1_filtrado_pb))
+        array_psnr_pb.append(calcular_psnr(q2_original, q2_filtrado_pb))
+        array_psnr_pb.append(calcular_psnr(q3_original, q3_filtrado_pb))
+        array_psnr_pb.append(calcular_psnr(q4_original, q4_filtrado_pb))
+        mediana_psnr_pb = round(np.median(array_psnr_pb), 2)
+
+        array_psnr_pa = []
+        array_psnr_pa.append(calcular_psnr(q1_original, q1_filtrado_pa))
+        array_psnr_pa.append(calcular_psnr(q2_original, q2_filtrado_pa))
+        array_psnr_pa.append(calcular_psnr(q3_original, q3_filtrado_pa))
+        array_psnr_pa.append(calcular_psnr(q4_original, q4_filtrado_pa))
+        mediana_psnr_pa = round(np.median(array_psnr_pa), 2)
 
         array_mse = []
-        array_mse.append(calcular_mse(q1_original, q1_filtrado))
-        array_mse.append(calcular_mse(q2_original, q2_filtrado))
-        array_mse.append(calcular_mse(q3_original, q3_filtrado))
-        array_mse.append(calcular_mse(q4_original, q4_filtrado))
+        array_mse.append(calcular_mse(q1_original, q1_filtrado_pb))
+        array_mse.append(calcular_mse(q2_original, q2_filtrado_pb))
+        array_mse.append(calcular_mse(q3_original, q3_filtrado_pb))
+        array_mse.append(calcular_mse(q4_original, q4_filtrado_pb))
         mediana_mse = round(np.median(array_mse), 2)
 
-        lista_porcentagem_corte = [0.05, 0.07, 0.10, 0.13, 0.15]
+        #lista_porcentagem_corte = [0.05, 0.07, 0.10, 0.13, 0.15]
         if porcentagem_corte == 0.05:
-            lista_psnr_filtro_5.append(round(np.median(array_psnr), 2))
-            lista_mse_filtro_5.append(round(np.median(array_mse), 2))
+            lista_psnr_filtro_5_pb.append(round(np.median(array_psnr_pb), 2))
+            lista_psnr_filtro_5_pa.append(round(np.median(array_psnr_pa), 2))
+            #lista_mse_filtro_5_pb.append(round(np.median(array_mse), 2))
         elif porcentagem_corte == 0.07:
-            lista_psnr_filtro_7.append(round(np.median(array_psnr), 2))
+            lista_psnr_filtro_7_pb.append(round(np.median(array_psnr_pb), 2))
+            lista_psnr_filtro_7_pa.append(round(np.median(array_psnr_pa), 2))
             #lista_mse_filtro_7.append(round(np.median(array_mse), 2))
         elif porcentagem_corte == 0.10:
-            lista_psnr_filtro_10.append(round(np.median(array_psnr), 2))
-            lista_mse_filtro_10.append(round(np.median(array_mse), 2))
+            lista_psnr_filtro_10_pb.append(round(np.median(array_psnr_pb), 2))
+            lista_psnr_filtro_10_pa.append(round(np.median(array_psnr_pa), 2))
         elif porcentagem_corte == 0.13:
-            lista_psnr_filtro_13.append(round(np.median(array_psnr), 2))
-            #lista_mse_filtro_10.append(round(np.median(array_mse), 2))
+            lista_psnr_filtro_13_pb.append(round(np.median(array_psnr_pb), 2))
+            lista_psnr_filtro_13_pa.append(round(np.median(array_psnr_pa), 2))
         else:
-            lista_psnr_filtro_15.append(round(np.median(array_psnr), 2))
-            lista_mse_filtro_15.append(round(np.median(array_mse), 2))
+            lista_psnr_filtro_15_pb.append(round(np.median(array_psnr_pb), 2))
+            lista_psnr_filtro_15_pa.append(round(np.median(array_psnr_pa), 2))
 
-        print('PSNR ('+ str(porcentagem_corte) +'): ' + str(mediana_psnr))
-        print('MSE: ('+ str(porcentagem_corte) +'): ' + str(mediana_mse))
-        print()
+        print('PSNR FILTRO PASSA BAIXA ('+ str(porcentagem_corte) +'): ' + str(mediana_psnr_pb))
+        print('PSNR FILTRO PASSA ALTA ('+ str(porcentagem_corte) +'): ' + str(mediana_psnr_pa))
+        #print()
 
 
 
         pylab.subplot(lin, col, pos), pylab.title('Imagem Filtrada (' + str(round(porcentagem_corte*100, 2)) + '%)')
         pylab.axis('off')
-        pylab.imshow(imagem_filtrada, cmap='gray')
+        pylab.imshow(imagem_filtrada_pb, cmap='gray')
         pos += 1
 
 
     #pylab.show()
-    #pylab.close()
+    pylab.close()
 
     #print(imagem_ruidosa.dtype)
     #print(imagem_filtrada.dtype)
 
     print()
 
-print('lista_psnr_filtro_5: ', str(lista_psnr_filtro_5))
-print('lista_psnr_filtro_7: ', str(lista_psnr_filtro_7))
-print('lista_psnr_filtro_10: ', str(lista_psnr_filtro_10))
-print('lista_psnr_filtro_13: ', str(lista_psnr_filtro_13))
-print('lista_psnr_filtro_15: ', str(lista_psnr_filtro_15))
+#print('lista_psnr_filtro_5: ', str(lista_psnr_filtro_5))
+#print('lista_psnr_filtro_7: ', str(lista_psnr_filtro_7))
+#print('lista_psnr_filtro_10: ', str(lista_psnr_filtro_10))
+#print('lista_psnr_filtro_13: ', str(lista_psnr_filtro_13))
+#print('lista_psnr_filtro_15: ', str(lista_psnr_filtro_15))
 
-print('Filtro 5%: Menor Valor: ' + str(np.min(lista_psnr_filtro_5)) + '. Maior Valor: ' + str(np.max(lista_psnr_filtro_5)))
-print('Filtro 7%: Menor Valor: ' + str(np.min(lista_psnr_filtro_7)) + '. Maior Valor: ' + str(np.max(lista_psnr_filtro_7)))
-print('Filtro 10%: Menor Valor: ' + str(np.min(lista_psnr_filtro_10)) + '. Maior Valor: ' + str(np.max(lista_psnr_filtro_10)))
-print('Filtro 13%: Menor Valor: ' + str(np.min(lista_psnr_filtro_13)) + '. Maior Valor: ' + str(np.max(lista_psnr_filtro_13)))
-print('Filtro 15%: Menor Valor: ' + str(np.min(lista_psnr_filtro_15)) + '. Maior Valor: ' + str(np.max(lista_psnr_filtro_15)))
+#print('Filtro 5%: Menor Valor: ' + str(np.min(lista_psnr_filtro_5)) + '. Maior Valor: ' + str(np.max(lista_psnr_filtro_5)))
+#print('Filtro 7%: Menor Valor: ' + str(np.min(lista_psnr_filtro_7)) + '. Maior Valor: ' + str(np.max(lista_psnr_filtro_7)))
+#print('Filtro 10%: Menor Valor: ' + str(np.min(lista_psnr_filtro_10)) + '. Maior Valor: ' + str(np.max(lista_psnr_filtro_10)))
+#print('Filtro 13%: Menor Valor: ' + str(np.min(lista_psnr_filtro_13)) + '. Maior Valor: ' + str(np.max(lista_psnr_filtro_13)))
+#print('Filtro 15%: Menor Valor: ' + str(np.min(lista_psnr_filtro_15)) + '. Maior Valor: ' + str(np.max(lista_psnr_filtro_15)))
 
-for idx in range(len(lista_psnr_filtro_5)):
-    psnr_5 = lista_psnr_filtro_5[idx]
-    psnr_7 = lista_psnr_filtro_7[idx]
-    psnr_10 = lista_psnr_filtro_10[idx]
-    psnr_13 = lista_psnr_filtro_13[idx]
-    psnr_15 = lista_psnr_filtro_15[idx]
+idx_pb = 0
+idx_pa = 0
 
-    if psnr_5 > psnr_7:
-        if psnr_5 > psnr_10:
-            if psnr_5 > psnr_13:
-                if psnr_5 > psnr_15:
-                    print('psnr 5% é a maior.')
+for idx in range(len(lista_psnr_filtro_5_pb)):
+    psnr_5_pb = lista_psnr_filtro_5_pb[idx]
+    psnr_7_pb = lista_psnr_filtro_7_pb[idx]
+    psnr_10_pb = lista_psnr_filtro_10_pb[idx]
+    psnr_13_pb = lista_psnr_filtro_13_pb[idx]
+    psnr_15_pb = lista_psnr_filtro_15_pb[idx]
 
-    if psnr_7 > psnr_10:
-        if psnr_7 > psnr_13:
-            if psnr_7 > psnr_15:
-                print('psnr 7% é a maior.')
+    psnr_5_pa = lista_psnr_filtro_5_pa[idx]
+    psnr_7_pa = lista_psnr_filtro_7_pa[idx]
+    psnr_10_pa = lista_psnr_filtro_10_pa[idx]
+    psnr_13_pa = lista_psnr_filtro_13_pa[idx]
+    psnr_15_pa = lista_psnr_filtro_15_pa[idx]
 
-    if psnr_10 > psnr_13:
-        if psnr_10 > psnr_15:
-            print('psnr 10% é a maior.')
+    if psnr_5_pb > psnr_7_pb:
+        if psnr_5_pb > psnr_10_pb:
+            if psnr_5_pb > psnr_13_pb:
+                if psnr_5_pb > psnr_15_pb:
+                    idx_pb = idx
+                    print('psnr pb 5% é a maior.')
+    if psnr_5_pa > psnr_7_pa:
+        if psnr_5_pa > psnr_10_pa:
+            if psnr_5_pa > psnr_13_pa:
+                if psnr_5_pa > psnr_15_pa:
+                    idx_pb = idx
+                    print('psnr pa 5% é a maior.')
 
-    if psnr_13 > psnr_15:
-        print('psnr 13% é a maior.')
+    if psnr_7_pa > psnr_10_pa:
+        if psnr_7_pa > psnr_13_pa:
+            if psnr_7_pa > psnr_15_pa:
+                idx_pb = idx
+                print('psnr pa 7% é a maior.')
+
+    if psnr_10_pa > psnr_13_pa:
+        if psnr_10_pa > psnr_15_pa:
+            print('psnr pa 10% é a maior.')
+
+    if psnr_13_pa > psnr_15_pa:
+        print('psnr pa 13% é a maior.')
     else:
-        print('psnr 15% é a maior.')
+        print('psnr pa 15% é a maior.')
+
+print('Total de Imagens: ' + str(len(lista_imagens)))
+print('Total PSNR FPB 5%:' + str(len(lista_psnr_filtro_5_pb)))
+print('Total PSNR FPA 5%:' + str(len(lista_psnr_filtro_5_pa)))
+
 
 
 print('FIM ESCOLHER MELHOR FILTRO')
